d62f6b1263b937128354143d01baad5d5d0b32ef
Edward <example@example.com>
2025-12-15 12:32:03 +0000

    moved code to source file

--- worlds-best-text-editor-3/application_configuration/src/application_configuration.cpp
+++ worlds-best-text-editor-3/application_configuration/src/application_configuration.cpp
@@ -4,5 +4,40 @@
 
 #include <json/json.h>
 
+std::optional<std::string> ApplicationConfiguration::getFontPath() const {
 
+    #ifdef _WIN32
+        const auto FONT_PATH_KEY = "font_path_windows";
+    #else
+        const auto FONT_PATH_KEY = "font_path_linux";
+    #endif
+
+    if (root.isMember(FONT_PATH_KEY)) {
+        std::string font_path = root[FONT_PATH_KEY].asString();
+        return font_path;
+    }
+
+    SPDLOG_ERROR("failed to get font path from configuration file");
+    return std::nullopt;
+}
+
+std::expected<ApplicationConfiguration, ApplicationConfigurationInitializationError>
+initialize_application_configuration() {
+    try {
+        const auto config = ApplicationConfiguration();
+        return config;
+    }
+    catch (ApplicationConfigurationInitializationFileDoesNotExistError &error) {
+        SPDLOG_ERROR("failed to initialize application configuration, file does not exist: {}", error.what());
+        return std::unexpected(ApplicationConfigurationInitializationError::FILE_DOES_NOT_EXIST);
+    }
+    catch (ApplicationConfigurationInitializationJsonParseError &error) {
+        SPDLOG_ERROR("failed to initialize application configuration, parsing json data failed: {}", error.what());
+        return std::unexpected(ApplicationConfigurationInitializationError::JSON_PARSE_ERROR);
+    }
+    catch (...) {
+        SPDLOG_ERROR("unhandled error in initialize_application_configuration");
+        throw;
+    }
+}
 
--- worlds-best-text-editor-3/application_configuration/src/application_configuration.hpp
+++ worlds-best-text-editor-3/application_configuration/src/application_configuration.hpp
@@ -48,22 +48,7 @@ struct ApplicationConfiguration {
         }
     }
 
-    std::optional<std::string> getFontPath() const {
-
-        #ifdef _WIN32
-            const auto FONT_PATH_KEY = "font_path_windows";
-        #else
-            const auto FONT_PATH_KEY = "font_path_linux";
-        #endif
-
-        if (root.isMember(FONT_PATH_KEY)) {
-            std::string font_path = root[FONT_PATH_KEY].asString();
-            return font_path;
-        }
-
-        SPDLOG_ERROR("failed to get font path from configuration file");
-        return std::nullopt;
-    }
+    std::optional<std::string> getFontPath() const;
 
     Json::Value root;
 };
@@ -74,24 +59,7 @@ enum class ApplicationConfigurationInitializationError {
 };
 
 std::expected<ApplicationConfiguration, ApplicationConfigurationInitializationError>
-initialize_application_configuration() {
-    try {
-        const auto config = ApplicationConfiguration();
-        return config;
-    }
-    catch (ApplicationConfigurationInitializationFileDoesNotExistError &error) {
-        SPDLOG_ERROR("failed to initialize application configuration, file does not exist: {}", error.what());
-        return std::unexpected(ApplicationConfigurationInitializationError::FILE_DOES_NOT_EXIST);
-    }
-    catch (ApplicationConfigurationInitializationJsonParseError &error) {
-        SPDLOG_ERROR("failed to initialize application configuration, parsing json data failed: {}", error.what());
-        return std::unexpected(ApplicationConfigurationInitializationError::JSON_PARSE_ERROR);
-    }
-    catch (...) {
-        SPDLOG_ERROR("unhandled error in initialize_application_configuration");
-        throw;
-    }
-}
+initialize_application_configuration();
 
 
 #endif // APPLICATION_CONFIGURATION_HPP
